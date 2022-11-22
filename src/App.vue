1b20ce92f403c2badf4b25f25553a23db994aa97
DanielTavares33 <danieltvs33@gmail.com>
2022-11-22 20:21:56 +0000

    add clear section for content and category

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+  <script setup>
 import { ref, onMounted, computed, watch } from "vue";
 
 const todos = ref([])
@@ -22,6 +22,9 @@ const addTodo = () => {
     done: false,
     createdAt: new Date().getTime()
   })
+
+  input_content.value = ''
+  input_category.value = ''
 }
 
 const removeTodo = (todo) => {
